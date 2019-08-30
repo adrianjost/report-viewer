b631ae19fa7deafe807ccbd6c82aa09c065ac017
Adrian Jost <adrianjost@users.noreply.github.com>
2019-08-30 15:36:04 +0200

    fix? caching regex

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -8,7 +8,7 @@ importScripts(
 // Cache First for reports
 workbox.routing.registerRoute(
 	new RegExp(
-		`https://europe-west1-ci-report-viewer.cloudfunctions.net/proxy/v1_0/.*`
+		`https://europe-west1-ci-report-viewer\\.cloudfunctions\\.net/proxy/v1_0/.*`
 	),
 	new workbox.strategies.CacheFirst({ cacheName: "reports" })
 );
